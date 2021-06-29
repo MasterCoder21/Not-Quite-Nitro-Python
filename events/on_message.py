c5fcce6dbd15a334afed7565a91bd2008d3e9ca5
MasterCoder21 <47282021+MasterCoder21@users.noreply.github.com>
2021-06-28 20:15:21 -0700

    Updating on_message.py to fix bug
    I noticed that this code wasn't working.  I saw that the stripping was wrong, and it wasn't getting the arguments as expected.  This should work.

--- events/on_message.py
+++ events/on_message.py
@@ -2,101 +2,87 @@ from discord.ext import commands
 from discord import utils
 import discord
 
+
 class emoji(commands.Cog):
-	def __init__(self, bot):
-		self.bot = bot
+    def __init__(self, bot):
+        self.bot = bot
 
-	async def getemote(self, arg):
-		emoji = utils.get(self.bot.emojis, name = arg.strip(":"))
+    async def getemote(self, arg:commands.EmojiConverter):
+        # emoji = utils.get(self.bot.emojis, name=arg.strip(":"))
 
-		if emoji is not None:
-			if emoji.animated:
-				add = "a"
-			else:
-				add = ""
-			return f"<{add}:{emoji.name}:{emoji.id}>"
-		else:
-			return None
+        return arg
 
-	async def getinstr(self, content):
-		ret = []
+    async def getinstr(self, content):
+        ret = []
 
-		spc = content.split(" ")
-		cnt = content.split(":")
+        spc = content.split(" ")
+        cnt = content.split(":")
 
-		if len(cnt) > 1:
-			for item in spc:
-				if item.count(":") > 1:
-					wr = ""
-					if item.startswith("<") and item.endswith(">"):
-						ret.append(item)
-					else:
-						cnt = 0
-						for i in item:
-							if cnt == 2:
-								aaa = wr.replace(" ", "")
-								ret.append(aaa)
-								wr = ""
-								cnt = 0
+        if len(cnt) > 1:
+            for item in spc:
+                if item.count(":") > 1:
+                    wr = ""
+                    if item.startswith("<") and item.endswith(">"):
+                        ret.append(item)
+                    else:
+                        cnt = 0
+                        for i in item:
+                            if cnt == 2:
+                                aaa = wr.replace(" ", "")
+                                ret.append(aaa)
+                                wr = ""
+                                cnt = 0
 
-							if i != ":":
-								wr += i
-							else:
-								if wr == "" or cnt == 1:
-									wr += " : "
-									cnt += 1
-								else:
-									aaa = wr.replace(" ", "")
-									ret.append(aaa)
-									wr = ":"
-									cnt = 1
+                            if i != ":":
+                                wr += i
+                            else:
+                                if wr == "" or cnt == 1:
+                                    wr += " : "
+                                    cnt += 1
+                                else:
+                                    aaa = wr.replace(" ", "")
+                                    ret.append(aaa)
+                                    wr = ":"
+                                    cnt = 1
 
-						aaa = wr.replace(" ", "")
-						ret.append(aaa)
-				else:
-					ret.append(item)
-		else:
-			return content
+                        aaa = wr.replace(" ", "")
+                        ret.append(aaa)
+                else:
+                    ret.append(item)
+        else:
+            return content
 
-		return ret
+        return ret
+
+    # i added extra indent by mistake -_-
+
+    @commands.Cog.listener()
+    async def on_message(self, message):
+        if message.author.bot:
+            return
+
+        if ":" in message.content:
+            msg = [i for i in message.content.split(" ")]
+            ret = ""
+            em = False
+            for word in msg:
+                if word.startswith("<") and word.endswith(">"):
+                    emoji = await self.getemote(word)
+                    ret += f"{emoji} "
+                else:
+                    ret += f"{word} "
 
 
-	# i added extra indent by mistake -_-
+            webhooks = await message.channel.webhooks()
+            webhook = utils.get(webhooks, name="Imposter NQN")
+            if webhook is None:
+                webhook = await message.channel.create_webhook(name="Imposter NQN")
 
-	@commands.Cog.listener()
-	async def on_message(self, message):
-		if message.author.bot:
-			return
+            if len(ret) == 0:
+                return
+            await webhook.send(ret, username=message.author.name, avatar_url=message.author.avatar_url)
+            await message.delete()
 
-		if ":" in message.content:
-			msg = await self.getinstr(message.content)
-			ret = ""
-			em = False
-			smth = message.content.split(":")
-			if len(smth) > 1:
-				for word in msg:
-					if word.startswith(":") and word.endswith(":") and len(word) > 1:
-						emoji = await self.getemote(word)
-						if emoji is not None:
-							em = True
-							ret += f" {emoji}"
-						else:
-							ret += f" {word}"
-					else:
-						ret += f" {word}"
-
-			else:
-				ret += msg
-			
-
-			if em:
-				webhooks = await message.channel.webhooks()
-				webhook = utils.get(webhooks, name = "Imposter NQN")
-				if webhook is None:
-					webhook = await message.channel.create_webhook(name = "Imposter NQN")
-
-				await webhook.send(ret, username = message.author.name, avatar_url = message.author.avatar_url)
-				await message.delete()
 
 def setup(bot):
-	bot.add_cog(emoji(bot))
+    bot.add_cog(emoji(bot))
